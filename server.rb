5a28acaa925489571e28197b331d36f61c9b7b48
rpechayr <rpechayr@gmail.com>
2014-10-10 16:40:34 +0200

    Add metadata as fields

--- server.rb
+++ server.rb
@@ -7,15 +7,15 @@ API_AUTH_TOKEN = ENV["MOBICHECKIN_AUTH_TOKEN"]
 #HOST = "http://localhost:5000"
 HOST = ENV["MOBICHECKIN_HOST"]
 
-metadatas = ["email", "uid", "order_uid", "first_name", "last_name", "company_name", "position", "phone_number", "message"]
+fields = ["email", "uid", "order_uid", "first_name", "last_name", "company_name", "position", "phone_number", "message", "guest_metadata"]
 
 post "/events/:target_event_id/guest_categories/:guest_category_id/guests/handle_hook" do |target_event_id, guest_category_id|
   parameters = JSON.parse(request.body.read)
   guest = parameters["guest"]
 
   options = {"guest_category_id" => guest_category_id}
-  metadatas.each do |metadata_name|
-    options.merge!(metadata_name => guest[metadata_name]) unless guest[metadata_name].nil?
+  fields.each do |field_name|
+    options.merge!(field_name => guest[field_name]) unless guest[field_name].nil?
   end
 
   response = HTTParty.post("#{HOST}/api/v1/events/#{target_event_id}/guests.json?auth_token=#{API_AUTH_TOKEN}", :body => {"guest" => options})
